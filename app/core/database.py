a0f89df994c179fdd1dd33a725540e90e8624fb2
Trilochan Chakrabortty <your-github-email@gmail.com>
2026-05-07 12:47:48 +0530

    Switched to SQLite for deployment

--- app/core/database.py
+++ app/core/database.py
@@ -1,11 +1,33 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DATABASE_URL = "mysql+pymysql://root:Trilochan%40123@localhost:3306/ai_agent_db"
+
+# engine = create_engine(DATABASE_URL, echo=False)
+
+# SessionLocal = sessionmaker(bind=engine)
+
+# Base = declarative_base()
+# print("DB URL:", DATABASE_URL)
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:Trilochan%40123@localhost:3306/ai_agent_db"
+# SQLite database
+DATABASE_URL = "sqlite:///./ai_agent.db"
 
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
-print("DB URL:", DATABASE_URL)
+
+print("SQLite DB Connected")
